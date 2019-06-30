f9ffe1f2131f7d92b60f92ddf9f946e62eb828ed
Jay Mannapperuma <mj7007@gmail.com>
2019-06-30 13:53:15 +1200

    Created BurgerBuilder component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const layout = props => (
   <Aux>
     <div>Toolbar, Side Drawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class Burgerbuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default Burgerbuilder;
