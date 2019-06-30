611426f23d07ffb14d97f191aec841d0024d2186
Jay Mannapperuma <mj7007@gmail.com>
2019-06-30 13:41:09 +1200

    Created Layout component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <Layout>Test</Layout>
+      </div>
+    );
   }
 }
 
