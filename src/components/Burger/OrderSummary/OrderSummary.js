4e8e7e438f31816b23b4d8253b6552113a7e6ea4
Jay Mannapperuma <mj7007@gmail.com>
2019-06-30 22:40:05 +1200

    Added total price to order summary modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,6 +17,9 @@ const orderSummary = props => {
       <h3>Your Order</h3>
       <p>A delicious burger with following ingredients:</p>
       <ul>{list}</ul>
+      <p>
+        <strong>Total Price : ${props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to checkout?</p>
       <Button btnType="Success" clicked={props.purchaseContinued}>
         CONTINUE
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -108,6 +108,7 @@ class Burgerbuilder extends Component {
         >
           <OrderSummary
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
             purchaseCancelled={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler}
           />
