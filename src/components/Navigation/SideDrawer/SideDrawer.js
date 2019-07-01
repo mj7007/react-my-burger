64981c32d78880b2f166fd1b377ba5fd3c6cc50c
Jay Mannapperuma <mj7007@gmail.com>
2019-07-01 23:04:25 +1200

    Implemented the side drawer component

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import classes from './SideDrawer.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+const sideDrawer = props => (
+  <Aux>
+    <Backdrop show={props.open} clicked={props.closed} />
+    <div
+      className={[
+        classes.SideDrawer,
+        props.open ? classes.Open : classes.Close
+      ].join(' ')}
+    >
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  </Aux>
+);
+
+export default sideDrawer;
