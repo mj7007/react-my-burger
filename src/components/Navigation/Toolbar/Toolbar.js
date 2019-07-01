483d757bb00b379c5f51a6ebcaa9fa711395e02e
Jay Mannapperuma <mj7007@gmail.com>
2019-07-01 23:53:15 +1200

    Tweaked CSS to improve responsiveness

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,13 +7,17 @@ import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={classes.DrawerToggleContainer}>
+      <DrawerToggle clicked={props.drawerToggleClicked} />
+    </div>
     <div className={classes.Logo}>
       <Logo />
     </div>
-    <nav className={classes.DesktopOnly}>
-      <NavigationItems />
-    </nav>
+    <div className={classes.NavigationContainer}>
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
   </header>
 );
 
